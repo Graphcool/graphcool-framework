63ab32361e3fff0c1f842954153c02ef8fd3518a
Marcus Böhm <wif.mboehm@gmx.de>
2017-11-17 18:09:05 +0100

    fix simple injector

--- server/backend-api-simple/src/main/scala/cool/graph/client/schema/simple/SimpleApiDependencies.scala
+++ server/backend-api-simple/src/main/scala/cool/graph/client/schema/simple/SimpleApiDependencies.scala
@@ -2,11 +2,7 @@ package cool.graph.client.schema.simple
 
 import akka.actor.{ActorSystem, Props}
 import akka.stream.ActorMaterializer
-import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.services.kinesis.{AmazonKinesis, AmazonKinesisClientBuilder}
-import cool.graph.aws.AwsInitializers
-import cool.graph.aws.cloudwatch.CloudwatchImpl
+import cool.graph.aws.cloudwatch.CloudwatchMock
 import cool.graph.client.database.{DeferredResolverProvider, SimpleManyModelDeferredResolver, SimpleToManyDeferredResolver}
 import cool.graph.client.finder.{CachedProjectFetcherImpl, ProjectFetcherImpl, RefreshableProjectFetcher}
 import cool.graph.client.metrics.ApiMetricsMiddleware
@@ -17,7 +13,7 @@ import cool.graph.messagebus.pubsub.rabbit.{RabbitAkkaPubSub, RabbitAkkaPubSubSu
 import cool.graph.messagebus.queue.rabbit.RabbitQueue
 import cool.graph.messagebus.{Conversions, PubSubPublisher, QueuePublisher}
 import cool.graph.shared.database.GlobalDatabaseManager
-import cool.graph.shared.externalServices.{KinesisPublisher, KinesisPublisherImplementation}
+import cool.graph.shared.externalServices.{DummyKinesisPublisher, KinesisPublisher}
 import cool.graph.shared.functions.lambda.LambdaFunctionEnvironment
 import cool.graph.shared.functions.{EndpointResolver, FunctionEnvironment, LiveEndpointResolver}
 import cool.graph.webhook.Webhook
@@ -66,16 +62,6 @@ case class SimpleApiDependencies(implicit val system: ActorSystem, val materiali
     projectSchemaInvalidationSubscriber = projectSchemaInvalidationSubscriber
   )
 
-  lazy val kinesis: AmazonKinesis = {
-    val credentials =
-      new BasicAWSCredentials(sys.env("AWS_ACCESS_KEY_ID"), sys.env("AWS_SECRET_ACCESS_KEY"))
-
-    AmazonKinesisClientBuilder.standard
-      .withCredentials(new AWSStaticCredentialsProvider(credentials))
-      .withEndpointConfiguration(new EndpointConfiguration(sys.env("KINESIS_ENDPOINT"), sys.env("AWS_REGION")))
-      .build
-  }
-
   lazy val clusterLocalRabbitUri              = sys.env("RABBITMQ_URI")
   lazy val fromStringMarshaller               = Conversions.Marshallers.FromString
   lazy val globalDatabaseManager              = GlobalDatabaseManager.initializeForSingleRegion(config)
@@ -84,19 +70,16 @@ case class SimpleApiDependencies(implicit val system: ActorSystem, val materiali
   lazy val webhooksPublisher                  = RabbitQueue.publisher(clusterLocalRabbitUri, "webhooks")(bugSnagger, Webhook.marshaller)
   lazy val sssEventsPublisher                 = RabbitAkkaPubSub.publisher[String](sys.env("RABBITMQ_URI"), "sss-events", durable = true)(bugSnagger, fromStringMarshaller)
   lazy val requestPrefix                      = sys.env.getOrElse("AWS_REGION", sys.error("AWS Region not found."))
-  lazy val cloudwatch                         = CloudwatchImpl()
-  lazy val kinesisAlgoliaSyncQueriesPublisher = new KinesisPublisherImplementation(streamName = sys.env("KINESIS_STREAM_ALGOLIA_SYNC_QUERY"), kinesis)
-  lazy val kinesisApiMetricsPublisher         = new KinesisPublisherImplementation(streamName = sys.env("KINESIS_STREAM_API_METRICS"), kinesis)
+  lazy val cloudwatch                         = CloudwatchMock
+  lazy val kinesisAlgoliaSyncQueriesPublisher = DummyKinesisPublisher()
+  lazy val kinesisApiMetricsPublisher         = DummyKinesisPublisher()
   lazy val featureMetricActor                 = system.actorOf(Props(new FeatureMetricActor(kinesisApiMetricsPublisher, apiMetricsFlushInterval)))
   lazy val apiMetricsMiddleware               = new ApiMetricsMiddleware(testableTime, featureMetricActor)
 
   binding identifiedBy "project-schema-fetcher" toNonLazy projectSchemaFetcher
   binding identifiedBy "cloudwatch" toNonLazy cloudwatch
-  binding identifiedBy "kinesis" toNonLazy kinesis
   binding identifiedBy "api-metrics-middleware" toNonLazy new ApiMetricsMiddleware(testableTime, featureMetricActor)
   binding identifiedBy "featureMetricActor" to featureMetricActor
-  binding identifiedBy "s3" toNonLazy AwsInitializers.createS3()
-  binding identifiedBy "s3-fileupload" toNonLazy AwsInitializers.createS3Fileupload()
 
   bind[FunctionEnvironment] toNonLazy functionEnvironment
   bind[EndpointResolver] identifiedBy "endpointResolver" toNonLazy endpointResolver
