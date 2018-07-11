aff6da28fe86bf6cd86791bfd305337e8bda6061
Dominic Petrick <dompetrick@gmail.com>
2018-07-11 19:43:15 +0200

    Change invocation pattern for non-ejected projects

--- server/backend-shared/src/main/scala/cool/graph/shared/functions/lambda/LambdaFunctionEnvironment.scala
+++ server/backend-shared/src/main/scala/cool/graph/shared/functions/lambda/LambdaFunctionEnvironment.scala
@@ -91,7 +91,7 @@ case class LambdaFunctionEnvironment(accounts: Vector[LambdaDeploymentAccount]) 
   def deployInternal(project: Project, externalFile: ExternalFile, name: String): Future[DeployResponse] = {
     val key     = externalFile.url.split("\\?").head.split("/").last
     val account = accountForId(project.nextFunctionDeploymentAccount)
-    val runtime = if (project.isEjected) "nodejs8.10" else Runtime.Nodejs610.toString
+    val runtime = if (!project.isEjected) "nodejs8.10" else Runtime.Nodejs610.toString
 
     def create =
       account
@@ -142,7 +142,11 @@ case class LambdaFunctionEnvironment(accounts: Vector[LambdaDeploymentAccount]) 
   }
 
   def invoke(project: Project, name: String, event: String): Future[InvokeResponse] = {
-    val account = accountForId(project.activeFunctionDeploymentAccount)
+    val account = if (project.isEjected) {
+      accountForId(project.activeFunctionDeploymentAccount)
+    } else {
+      accountForId(project.nextFunctionDeploymentAccount)
+    }
 
     account
       .lambdaClient(project)
